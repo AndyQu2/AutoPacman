fa4faabba973b972c9e7efa70bb8e5531795ceca
AndyQu2 <s2230125@fms2.edu.hk>
2025-10-18 15:24:20 +0800

    Init commit

---
+++ q_network.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn
+
+class QNetwork(nn.Module):
+    def __init__(self, state_size, action_size, hidden_size=128):
+        super(QNetwork, self).__init__()
+        self.fc1 = nn.Linear(state_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, hidden_size)
+        self.fc4 = nn.Linear(hidden_size, hidden_size)
+        self.fc5 = nn.Linear(hidden_size, action_size)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = torch.relu(self.fc3(x))
+        x = torch.relu(self.fc4(x))
+        x = self.fc5(x)
+        return x
+
+class DeepQNetwork(nn.Module):
+    def __init__(self, args):
+        super(DeepQNetwork, self).__init__()
